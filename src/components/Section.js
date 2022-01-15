5e5bbbea6fecfeeffe64fa07a6730412526362c9
Fraser Redford <57697667+fredford@users.noreply.github.com>
2022-01-14 19:42:32 -0700

    added checkmarks

--- src/components/Section.js
+++ src/components/Section.js
@@ -19,6 +19,10 @@ export default function Section(props) {
     props.updateWeapon(section, index, "gold");
   };
 
+  const clickCamo = () => {
+    console.log("here");
+  };
+
   return (
     <div key={section}>
       <h2 className="">{section}</h2>
@@ -35,12 +39,22 @@ export default function Section(props) {
                 </div>
                 <div className="camo-progress">
                   {Object.keys(item.camos).map((key) => (
-                    <div key={key}>
+                    <div className="camo-container" key={key} onClick={() => clickCamo()}>
                       <img
                         src={camoFiles[key]}
                         alt={item.camos[key].description}
-                        className="camo"
+                        className="camo-image"
                       />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        className="bi bi-check checkrotate"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                      </svg>
                     </div>
                   ))}
                 </div>
