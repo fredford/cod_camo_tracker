7928657f7bf20345547841472d21e2a1fde38ede
Fraser Redford <57697667+fredford@users.noreply.github.com>
2022-01-27 10:19:11 -0700

    fixed variable names and camo fill on phones

--- src/components/Camo.js
+++ src/components/Camo.js
@@ -9,13 +9,13 @@ export default function Camo({ camo, id, onToggle, index }) {
     onToggle(camo, index);
   };
 
-  var showCheck = "camo-check bi bi-check-square-fill d-none";
+  var camoCheck = "camo-check bi bi-check-square-fill d-none";
 
-  var camoContainer = "camo-image ";
+  var camoImage = "camo-image ";
 
   if (camo.completion) {
-    showCheck = "camo-check bi bi-check-square-fill";
-    camoContainer += "d-none";
+    camoCheck = "camo-check bi bi-check-square-fill";
+    camoImage += "d-none";
   }
 
   return (
@@ -32,12 +32,12 @@ export default function Camo({ camo, id, onToggle, index }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="currentColor"
-          className={showCheck}
+          className={camoCheck}
           viewBox="0 0 16 16"
         >
           <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
         </svg>
-        <img className={camoContainer} src={camoPath} alt="camo" />
+        <img className={camoImage} src={camoPath} alt="camo" />
       </div>
     </div>
   );
